9f686149cc90a72f89d4f01d7f731a057918f7e4
Dias Nur Ramadhan <diasnurramadhan@gmail.com>
2023-02-18 15:50:40 +0700

    menu mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Search from "./Search";
 
 const Hero = () => {
   return (
-    <header className="flex flex-col gap-3 bg-[url('./images/BG.png')] bg-contain bg-center p-5 text-white">
+    <header className="flex flex-col gap-3 bg-[url('public/images/BG.png')] bg-contain bg-center p-5 text-white">
       <p className="text-base font-semibold ">Welcome to Bd Screens</p>
       <h1 className="w-3/4 text-2xl font-bold ">
         Download Unlimited Movies, Drama, Music Video and More Content.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-
+import Menu from "./Menu";
 const Navbar = () => {
   return (
-    <nav className="container mx-auto flex items-center justify-between p-5 ">
-      <a href="#">
-        <img
-          src={"/images/full-logo.svg"}
-          className="h-[26px] w-[107px]"
-          alt="logo"
-        />
-      </a>
+    <nav>
+      <div className="mx-auto flex items-center justify-between p-5 ">
+        <a href="#">
+          <img
+            src={"public/images/full-logo.svg"}
+            className="h-[26px] w-[107px]"
+            alt="logo"
+          />
+        </a>
 
-      <div className="flex items-center gap-4">
-        <MagnifyingGlassIcon className="h-5" />
-        <button className="flex h-9 items-center justify-center rounded-md bg-brand__red">
-          Sign in
-        </button>
+        <div className="flex items-center gap-4">
+          <MagnifyingGlassIcon className="h-5" />
+          <button className="flex h-9 items-center justify-center rounded-md bg-brand__red">
+            Sign in
+          </button>
+        </div>
       </div>
+
+      <Menu />
     </nav>
   );
 };
